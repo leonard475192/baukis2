7448d6733b4072e58aadde2a04c82ad423f1df02
leonard475192 <leonard.t1028@gmail.com>
2022-05-22 21:39:29 +0900

    feat: add before action

--- app/controllers/admin/staff_members_controller.rb
+++ app/controllers/admin/staff_members_controller.rb
@@ -35,12 +35,13 @@ class Admin::StaffMembersController < Admin::Base
     else
       render action: "edit"
     end
+  end
 
-    def destory
-      staff_member = StaffMember.find(params[:id])
-      staff_member.destory!
-      flash.notice = "職員アカウントを削除しました"
-      redirect_to :admin_staff_members
+  def destory
+    staff_member = StaffMember.find(params[:id])
+    staff_member.destory!
+    flash.notice = "職員アカウントを削除しました"
+    redirect_to :admin_staff_members
   end
 
   private def staff_member_params
